7e912cbf005551518752de362d54ddb5a76ad9e3
Odin Dutton <odindutton@gmail.com>
2014-06-27 22:03:46 +0200

    RSpec 3 support

--- lib/nc.rb
+++ lib/nc.rb
@@ -2,10 +2,14 @@ require 'rspec/core/formatters/base_text_formatter'
 require 'terminal-notifier'
 
 class Nc < RSpec::Core::Formatters::BaseTextFormatter
+  if RSpec::Core::Formatters.respond_to? :register
+    RSpec::Core::Formatters.register self.class, :example_failed
+  end
+
   def dump_summary(duration, example_count, failure_count, pending_count)
     body = []
-    body << "Finished in #{format_duration duration}"
-    body << summary_line(example_count, failure_count, pending_count)
+    body << "Finished in #{_format_duration duration}"
+    body << _summary_line(example_count, failure_count, pending_count)
 
     name = File.basename(File.expand_path '.')
 
@@ -27,4 +31,29 @@ class Nc < RSpec::Core::Formatters::BaseTextFormatter
   def say(title, body)
     TerminalNotifier.notify body, :title => title
   end
+
+  def _format_duration(duration)
+    if respond_to? :format_duration
+      format_duration duration
+    else
+      require 'rspec/core/formatters/helpers'
+      RSpec::Core::Formatters::Helpers.format_duration duration
+    end
+  end
+
+  def _summary_line(example_count, failure_count, pending_count)
+    if respond_to? :summary_line
+      summary_line(example_count, failure_count, pending_count)
+    else
+      RSpec::Core::Notifications::SummaryNotification.new(
+        nil,
+        SizeResponder.new(example_count),
+        SizeResponder.new(failure_count),
+        SizeResponder.new(pending_count),
+        nil
+      ).totals_line
+    end
+  end
+
+  SizeResponder = Struct.new(:size)
 end
--- lib/nc_fail.rb
+++ lib/nc_fail.rb
@@ -2,7 +2,12 @@ require 'nc'
 
 class NcFail < Nc
   def say(title, body)
+    @failed_examples ||= []
     return if @failed_examples.size <= 0
     super
+  end
+
+  def example_failed(failure)
+    @failed_examples << failure
   end
 end
